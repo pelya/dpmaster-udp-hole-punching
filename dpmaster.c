ceab2c1f88d0a0a18432d01cd0efa8cf91dd7647
molivier <molivier@d7cf8633-e32d-0410-b094-e92efae38249>
2002-10-14 06:24:26 +0000

    Fixed the binary name and removed an unused lib in MinGW makefile. Added command line options (port, testmode, max nb servers). Server records are now stored in linked lists indexed by a simple hash function. Localhost addresses are automatically translated into valid internet addresses. GetInfo challenges stay valid for a couple of seconds now, fixing a basic race condition. Fixed a few things for compilation with MinGW. Plus probably a few minor changes here and there I don't remember
    git-svn-id: svn://svn.icculus.org/twilight/trunk/dpmaster@2523 d7cf8633-e32d-0410-b094-e92efae38249

--- dpmaster.c
+++ dpmaster.c
@@ -31,22 +31,29 @@
 # include <winsock2.h>
 #else
 # include <arpa/inet.h>
+# include <netdb.h>
 # include <netinet/in.h>
 # include <sys/socket.h>
 # include <unistd.h>
 #endif
 
 
-// ------ Constants ------ //
+// ---------- Constants ---------- //
 
-// Maximum number of servers in the list
-#define MAX_NB_SERVERS 512
+// Version of dpmaster
+#define VERSION "1.0"
 
-// Number of characters in a challenge
-#define CHALLENGE_LENGTH 11
+// Maximum number of servers in all lists by default
+#define DEFAULT_MAX_NB_SERVERS 256
 
-// Number of characters for a gamename
-#define GAMENAME_LENGTH 63
+// Size of an address hash, in bits
+#define HASH_SIZE 8
+
+// Number of characters in a challenge, including the '\0'
+#define CHALLENGE_LENGTH 12
+
+// Max number of characters for a gamename, including the '\0'
+#define GAMENAME_LENGTH 64
 
 // Default master port
 #define DEFAULT_MASTER_PORT 27950
@@ -56,11 +63,12 @@
 #define MIN_PACKET_SIZE 5
 
 // Timeouts (in secondes)
+#define TIMEOUT_CHALLENGE    2
 #define TIMEOUT_HEARTBEAT    2
 #define TIMEOUT_INFORESPONSE (15 * 60)
 
 
-// Types of messages (with examples and description):
+// Types of messages (with examples and descriptions):
 
 // The heartbeat is sent by a server when it wants to get noticed by a master.
 // A server should send an heartbeat each time its state changes (new map,
@@ -105,11 +113,11 @@ Example of packet for "infoReponse" (Q3):
 #define M2C_GETSERVERSREPONSE "getserversResponse"
 
 
-// ------ Types ------ //
+// ---------- Types ---------- //
 
 // A few basic types
 typedef enum {false, true} qboolean;
-typedef unsigned char byte;
+typedef unsigned char qbyte;
 
 #ifdef WIN32
 typedef int socklen_t;
@@ -119,36 +127,53 @@ typedef int socklen_t;
 typedef enum {GAME_QUAKE3, GAME_DARKPLACES} game_t;
 
 // Server properties
-typedef struct
+typedef struct server_s
 {
+	struct server_s* next;
 	struct sockaddr_in address;
 	unsigned int protocol;
-	byte challenge [CHALLENGE_LENGTH + 1];
+	qbyte challenge [CHALLENGE_LENGTH];
 	unsigned short nbclients;
 	unsigned short maxclients;
 	time_t timeout;
+	time_t challenge_timeout;  // each challenge remains valid during a short time
 	game_t game;
-	byte gamename [GAMENAME_LENGTH + 1];  // DP only
+	qbyte gamename [GAMENAME_LENGTH];  // DP only
 } server_t;
 
 
-// ------ Global variables ------ //
+// ---------- Global variables ---------- //
 
-// The server list is sorted from the smallest IP address to the highest one
-// We make sure there's no hole into the list; the first "nbservers" slots
-// are used, and the others are not.
-// FIXME: well, it's not sorted yet...  :oP
-server_t servers [MAX_NB_SERVERS];
-size_t nbservers = 0;
+// All server structures are allocated in one block in the "servers" array.
+// Each used slot is also part of a linked list in "server_lists". A simple
+// hash of the address and port of the server gives the index of the list
+// used for a particular server.
+server_t* servers;
+server_t* server_lists [1 << HASH_SIZE];
+size_t max_nb_servers = DEFAULT_MAX_NB_SERVERS;
+size_t nb_servers = 0;
 
-// LordHavoc: print extra info about what is happening
-int testmode = 0;
+// Last allocated entry in the "servers" array.
+// Used as a start index for finding a free slot in "servers" more quickly
+size_t last_alloc;
+
+// Print extra info about what is happening
+qboolean testmode = false;
 
 // The master socket
 int sock;
 
+// The current time (updated every time we receive a packet)
+time_t crt_time;
 
-// ------ Functions ------ //
+// The port we use
+unsigned short master_port = DEFAULT_MASTER_PORT;
+
+// Our own address
+struct in_addr localaddr;
+
+
+// ---------- Functions ---------- //
 
 /*
 ====================
@@ -157,12 +182,15 @@ PrintPacket
 Print the contents of a packet on stdout
 ====================
 */
-static void PrintPacket (const byte* packet, size_t length)
+static void PrintPacket (const qbyte* packet, size_t length)
 {
 	size_t i;
+
+	putchar ('"');
+
 	for (i = 0; i < length; i++)
 	{
-		byte c = packet[i];
+		qbyte c = packet[i];
 		if (c == '\\')
 			printf ("\\\\");
 		else if (c >= 32 && c <= 127)
@@ -170,6 +198,8 @@ static void PrintPacket (const byte* packet, size_t length)
 		else
 			printf ("\\x%02X", c);
 	}
+
+	printf ("\" (%u bytes)\n", length);
 }
 
 /*
@@ -266,17 +296,127 @@ static qboolean SysInit (void)
 
 /*
 ====================
+ParseCommandLine
+
+Parse the options passed by the command line
+====================
+*/
+static qboolean ParseCommandLine (int argc, char* argv [])
+{
+	int ind = 1;
+	qboolean print_help = false;
+
+	while (ind < argc && !print_help)
+	{
+		// If it doesn't even look like an option, why bother?
+		if (argv[ind][0] != '-')
+			print_help = true;
+
+		else switch (argv[ind][1])
+		{
+			// Port number
+			case 'p':
+			{
+				unsigned short port_num = 0;
+				ind++;
+				if (ind < argc)
+					port_num = atoi (argv[ind]);
+				if (!port_num)
+					print_help = true;
+				else
+					master_port = port_num;
+				break;
+			}
+
+			// Maximum number of servers
+			case 'n':
+			{
+				size_t nb = 0;
+				ind++;
+				if (ind < argc)
+					nb = atoi (argv[ind]);
+				if (!nb)
+					print_help = true;
+				else
+					max_nb_servers = nb;
+				break;
+			}
+
+			// Test mode
+			case 't':
+				testmode = true;
+				break;
+
+			default:
+				print_help = true;
+		}
+		
+		ind++;
+	}
+	
+	if (print_help)
+	{
+		printf ("Syntax: dpmaster [options]\n"
+				"Options can be:\n"
+				"	-n <max_servers> : maximum number of servers recorded (default: %u)\n"
+				"	-p <port_num>    : listen on port <port_num> (default: %hu)\n"
+				"	-t               : test mode (print more informations, for debugging)\n",
+				DEFAULT_MAX_NB_SERVERS, DEFAULT_MASTER_PORT);
+
+		return false;
+	}
+
+	return true;
+}
+
+
+/*
+====================
 MasterInit
 
 System independent initializations
 ====================
 */
-static qboolean MasterInit (int argc, char* argv [])
+static qboolean MasterInit (void)
 {
 	struct sockaddr_in address;
+	char localname [256];
+	size_t array_size;
 
-	// Init the random seed
-	srand (time (NULL));
+	// Init the time and the random seed
+	crt_time = time (NULL);
+	srand (crt_time);
+
+	// Allocate "servers" and clean the arrays
+	array_size = max_nb_servers * sizeof (servers[0]);
+	servers = malloc (array_size);
+	if (!servers)
+	{
+		perror ("Can't allocate the servers array");
+		return false;
+	}
+	last_alloc = max_nb_servers - 1;
+	memset (servers, 0, array_size);
+	memset (server_lists, 0, sizeof (server_lists));
+	printf ("* %u server records allocated\n", max_nb_servers);
+	
+	// Get our own internet address (i.e. not 127.x.x.x)
+	localaddr.s_addr = 0;
+	if (!gethostname (localname, sizeof (localname)))
+	{
+		struct hostent *host = gethostbyname (localname);
+		if (host && host->h_addrtype == AF_INET && host->h_addr[0] != 127)
+		{
+			memcpy (&localaddr.s_addr, host->h_addr, sizeof (localaddr.s_addr));
+			printf ("* Local address resolved (%s => %u.%u.%u.%u)\n", localname,
+					host->h_addr[0] & 0xFF, host->h_addr[1] & 0xFF,
+					host->h_addr[2] & 0xFF, host->h_addr[3] & 0xFF);
+		}
+		else
+			printf ("* WARNING: can't determine the local host address\n");
+	}
+	else
+		printf ("* WARNING: can't determine the local host name\n");
 
 	// Open the socket
 	sock = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -290,7 +430,7 @@ static qboolean MasterInit (int argc, char* argv [])
     memset (&address, 0, sizeof (address));
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = htonl (INADDR_ANY);
-	address.sin_port = htons (DEFAULT_MASTER_PORT);
+	address.sin_port = htons (master_port);
 	if (bind (sock, (struct sockaddr*)&address, sizeof (address)) != 0)
 	{
 		perror ("Bind failed");
@@ -301,9 +441,24 @@ static qboolean MasterInit (int argc, char* argv [])
 #endif
 		return false;
 	}
+	printf ("* Listening on port %hu\n", ntohs (address.sin_port));
 
-	printf ("* Listening on port %hu\n\n", ntohs (address.sin_port));
 	return true;
+}
+
+
+/*
+====================
+AddressHash
+
+Compute the hash of a server address
+====================
+*/
+static unsigned int AddressHash (const struct sockaddr_in* address)
+{
+	qbyte* addr = (qbyte*)&address->sin_addr.s_addr;
+	qbyte* port = (qbyte*)&address->sin_port;
+	return (addr[0] ^ addr[1] ^ addr[2] ^ addr[3] ^ port[0] ^ port[1]);
 }
 
 
@@ -316,31 +471,56 @@ Search for a particular server in the list; add it if necessary
 */
 static server_t* GetServer (const struct sockaddr_in* address, qboolean add_it)
 {
-	size_t ind;
-	server_t* sv;
+	unsigned int hash = AddressHash (address);
+	server_t *prev = NULL, *sv = server_lists[hash];
+	unsigned int i;
 
-	for (ind = 0; ind < nbservers; ind++)
+	while (sv)
 	{
-		sv = &servers[ind];
 		if (sv->address.sin_addr.s_addr == address->sin_addr.s_addr &&
 			sv->address.sin_port == address->sin_port)
-			return sv;
-	}
+		{
+			// Put it on top of the list
+			// (useful because heartbeats are always followed by infoResponses)
+			if (prev)
+			{
+				prev->next = sv->next;
+				sv->next = server_lists[hash];
+				server_lists[hash] = sv;
+			}
 
+			return sv;
+		}
+
+		prev = sv;
+		sv = sv->next;
+	}
+	
 	if (!add_it)
 		return NULL;
 
 	// No more room?
-	if (nbservers == sizeof (servers) / sizeof (servers[0]))
+	if (nb_servers == max_nb_servers)
 		return NULL;
 
-	sv = &servers[nbservers];
+	// Look for the first free entry in "servers"
+	for (i = (last_alloc + 1) % max_nb_servers; i != last_alloc; i = (i + 1) % max_nb_servers)
+		if (! servers[i].address.sin_port)
+			break;
+	sv = &servers[i];
+	last_alloc = i;
+
+	// Initialize the structure
 	memset (sv, 0, sizeof (*sv));
 	memcpy (&sv->address, address, sizeof (*address));
-	sv->timeout = time (NULL) + 3;
+	sv->timeout = crt_time + 3;
 
-	printf ("  - Added to the server list (number %u)\n", nbservers);
-	nbservers++;
+	// Add it to the list it belongs
+	sv->next = server_lists[hash];
+	server_lists[hash] = sv;
+	nb_servers++;
+
+	printf ("  - Added to the server list (hash: 0x%02X)\n", hash);
 
 	return sv;
 }
@@ -355,7 +535,7 @@ Build a challenge string for a "getinfo" message
 */
 static const char* BuildChallenge (void)
 {
-	static char challenge [CHALLENGE_LENGTH + 1];
+	static char challenge [CHALLENGE_LENGTH];
 	size_t ind;
 	size_t length = sizeof (challenge) - 1;  // TODO: make it random?
 
@@ -384,19 +564,22 @@ Send a "getinfo" message to a server
 */
 static void SendGetInfo (server_t* server)
 {
-	byte msg [64] = "\xFF\xFF\xFF\xFFgetinfo ";
-	const char* challenge = BuildChallenge ();
+	qbyte msg [64] = "\xFF\xFF\xFF\xFFgetinfo ";
+	
+	if (!server->challenge_timeout || server->challenge_timeout < crt_time)
+	{
+		strcpy (server->challenge, BuildChallenge ());
+		server->challenge_timeout = crt_time + TIMEOUT_CHALLENGE;
+	}
 
-	strcat (msg, challenge);
+	strcat (msg, server->challenge);
 	sendto (sock, msg, strlen (msg), 0,
 			(const struct sockaddr*)&server->address,
 			sizeof (server->address));
 
-	strcpy (server->challenge, challenge);
-
 	printf ("* %s:%hu <--- getinfo with challenge \"%s\"\n",
 			inet_ntoa (server->address.sin_addr),
-			ntohs (server->address.sin_port), challenge);
+			ntohs (server->address.sin_port), server->challenge);
 }
 
 
@@ -407,10 +590,10 @@ HandleGetServers
 Parse getservers requests and send the appropriate response
 ====================
 */
-static void HandleGetServers (const byte* msg, const struct sockaddr_in* addr)
+static void HandleGetServers (const qbyte* msg, const struct sockaddr_in* addr)
 {
-	byte gamename [GAMENAME_LENGTH + 1] = "";
-	byte packet [2048] = "\xFF\xFF\xFF\xFFgetserversResponse\\";
+	qbyte gamename [GAMENAME_LENGTH] = "";
+	qbyte packet [2048] = "\xFF\xFF\xFF\xFFgetserversResponse\\";
 	size_t packetind = 23;  // = strlen (packet)
 	unsigned int protocol;
 	qboolean no_empty;
@@ -419,7 +602,9 @@ static void HandleGetServers (const byte* msg, const struct sockaddr_in* addr)
 	unsigned int sv_addr;
 	unsigned int sv_port;
 	game_t game;
-	time_t crttime = time (NULL);
+
+	printf ("* %s:%hu ---> getservers\n",
+			inet_ntoa (addr->sin_addr), ntohs (addr->sin_port));
 
 	// Check if there's a name before the protocol number
 	// In this case, the message comes from a DarkPlaces client
@@ -444,49 +629,56 @@ static void HandleGetServers (const byte* msg, const struct sockaddr_in* addr)
 	no_empty = (strstr (msg, "empty") == NULL);
 	no_full = (strstr (msg, "full") == NULL);
 
+	printf ("* %s:%hu <--- getserversResponse\n",
+			inet_ntoa (addr->sin_addr), ntohs (addr->sin_port));
+
 	// Add every relevant server
-	for (ind = 0; ind < nbservers; ind++)
+	for (ind = 0; ind < nb_servers; ind++)
 	{
 		server_t* sv = &servers[ind];
 
 		if (packetind >= sizeof (packet) - 12)
 			break;
 
-		if (sv->game != game)
+		if (sv->game != game)  // same game?
 			continue;
-		if (sv->protocol != protocol)
+		if (sv->protocol != protocol)  // same protocol?
 			continue;
-		if (sv->nbclients == 0 && no_empty)
+		if (sv->nbclients == 0 && no_empty)  // send empty servers?
 			continue;
-		if (sv->nbclients == sv->maxclients && no_full)
+		if (sv->nbclients == sv->maxclients && no_full)  // send full servers?
 			continue;
-		if (gamename[0] && strcmp (gamename, sv->gamename))
+		if (gamename[0] && strcmp (gamename, sv->gamename))  // same mod?
 			continue;
 
 		// The removal of old servers is done after the parsing,
 		// so we also need to check the timeout value here
-		if (sv->timeout < crttime)
+		if (sv->timeout < crt_time)
 			continue;
 
 		sv_addr = ntohl (sv->address.sin_addr.s_addr);
 		sv_port = ntohs (sv->address.sin_port);
-		if (testmode)
-			printf("sending server %d.%d.%d.%d:%d\n", (sv_addr >> 24) & 0xFF, (sv_addr >> 16) & 0xFF, (sv_addr >> 8) & 0xFF, sv_addr & 0xFF, sv_port);
 
 		// IP address
 		packet[packetind    ] =  sv_addr >> 24;
 		packet[packetind + 1] = (sv_addr >> 16) & 0xFF;
 		packet[packetind + 2] = (sv_addr >>  8) & 0xFF;
 		packet[packetind + 3] =  sv_addr        & 0xFF;
-		packetind += 4;
 
 		// Port
-		packet[packetind    ] = (sv_port >> 8) & 0xFF;
-		packet[packetind + 1] =  sv_port       & 0xFF;
-		packetind += 2;
+		packet[packetind + 4] = (sv_port >> 8) & 0xFF;
+		packet[packetind + 5] =  sv_port       & 0xFF;
 
 		// Trailing '\'
-		packet[packetind++] = '\\';
+		packet[packetind + 6] = '\\';
+
+		if (testmode)
+			printf ("  - Sending server %u.%u.%u.%u:%u\n",
+					packet[packetind    ], packet[packetind + 1],
+					packet[packetind + 2], packet[packetind + 3],
+					sv_port);
+
+		packetind += 7;
 	}
 
 	// End Of Transmission
@@ -498,10 +690,9 @@ static void HandleGetServers (const byte* msg, const struct sockaddr_in* addr)
 	packet[packetind + 5] = '\0';
 	packetind += 6;
 
-	// Print the packet contents on stdout
-	printf ("* %s:%hu <--- getserversResponse (%u bytes => %u servers)\n",
-			inet_ntoa (addr->sin_addr), ntohs (addr->sin_port),
-			packetind, (packetind - 22) / 7 - 1);
+	// Print a few more informations
+	printf ("  - %u servers in %u bytes\n",
+			(packetind - 22) / 7 - 1, packetind);
 
 	// Send the packet to the client
 	sendto (sock, packet, packetind, 0, (const struct sockaddr*)addr,
@@ -516,7 +707,7 @@ HandleInfoResponse
 Parse infoResponse messages
 ====================
 */
-static void HandleInfoResponse (server_t* server, const byte* msg)
+static void HandleInfoResponse (server_t* server, const qbyte* msg)
 {
 	char* value;
 
@@ -556,7 +747,7 @@ static void HandleInfoResponse (server_t* server, const byte* msg)
 	}
 
 	// Set a new timeout
-	server->timeout = time (NULL) + TIMEOUT_INFORESPONSE;
+	server->timeout = crt_time + TIMEOUT_INFORESPONSE;
 }
 
 
@@ -567,7 +758,7 @@ HandleMessage
 Parse a packet to figure out what to do with it
 ====================
 */
-static void HandleMessage (const byte* msg, size_t length,
+static void HandleMessage (const qbyte* msg, size_t length,
 						   const struct sockaddr_in* address)
 {
 	server_t* server;
@@ -605,7 +796,7 @@ static void HandleMessage (const byte* msg, size_t length,
 		server->game = game;
 
 		// Set the new timeout value
-		server->timeout = time (NULL) + TIMEOUT_HEARTBEAT;
+		server->timeout = crt_time + TIMEOUT_HEARTBEAT;
 
 		// Ask for some infos
 		SendGetInfo (server);
@@ -639,25 +830,32 @@ Check if some servers have reached their timeout limit
 void CheckTimeouts (void)
 {
 	unsigned int ind = 0;
-	time_t crttime = time (NULL);
-	unsigned int nbremoved = 0;
+	server_t *prev, *sv;
 
-	while (ind < nbservers)
+	// Browse every server list
+	for (ind = 0; ind < sizeof (server_lists) / sizeof (server_lists[0]); ind++)
 	{
-		// If the current server hasn't timed out, go check the next one
-		if (servers[ind].timeout >= crttime)
+		for (prev = NULL, sv = server_lists[ind]; sv != NULL; sv = sv->next)
 		{
-			ind++;
-			continue;
+			// If the current server has timed out, remove it
+			if (sv->timeout < crt_time)
+			{
+				if (prev)
+					prev->next = sv->next;
+				else
+					server_lists[ind] = sv->next;
+
+				nb_servers--;
+				printf ("* Server %s:%hu has timed out (%u servers remaining)\n",
+						inet_ntoa (sv->address.sin_addr),
+						ntohs (sv->address.sin_port), nb_servers);
+				
+				// Mark it as "free"
+				sv->address.sin_port = 0;
+			}
+			else
+				prev = sv;
 		}
-
-		if (ind < nbservers - 1)
-			memmove (&servers[ind], &servers[ind + 1],
-						(nbservers - ind - 1)  * sizeof (servers[ind]));
-
-		printf ("* Server %u timed out\n", ind + nbremoved);
-		nbservers--;
-		nbremoved++;
 	}
 }
 
@@ -674,17 +872,15 @@ int main (int argc, char* argv [])
 	struct sockaddr_in address;
 	socklen_t addrlen;
 	int nb_bytes;
-	byte packet [MAX_PACKET_SIZE + 1];  // "+ 1" because we append a '\0'
+	qbyte packet [MAX_PACKET_SIZE + 1];  // "+ 1" because we append a '\0'
 
 	printf ("\n"
 			"dpmaster, a master server for DarkPlaces and Q3A\n"
-			"(compiled the " __DATE__ " at " __TIME__ ")\n"
+			"(version " VERSION ", compiled the " __DATE__ " at " __TIME__ ")\n"
 			"\n");
 
 	// Initializations
-	if (!SysInit ())
-		return EXIT_FAILURE;
-	if (!MasterInit (argc, argv))
+	if (!ParseCommandLine (argc, argv) || !SysInit () || !MasterInit ())
 		return EXIT_FAILURE;
 
 	// Until the end of times...
@@ -696,17 +892,22 @@ int main (int argc, char* argv [])
 							 (struct sockaddr*)&address, &addrlen);
 		if (nb_bytes <= MIN_PACKET_SIZE || *((int*)packet) != 0xFFFFFFFF)
 		{
-			printf ("* ERROR: Recvfrom returned %d\n", nb_bytes);
+			printf ("* WARNING: \"recvfrom\" returned %d\n", nb_bytes);
 			continue;
 		}
 
 		// We append a '\0' to make the parsing easier
 		packet[nb_bytes] = '\0';
 
+		// We update the current time and print the packet if necessary
+		crt_time = time (NULL);
 		if (testmode)
 			PrintPacket (packet, nb_bytes);
-		// TODO: if the address is the loopback address, we should translate
-		//       it into a valid internet address
+
+		// If the sender address is the loopback address, we try
+		// to translate it into a valid internet address
+		if ((ntohl (address.sin_addr.s_addr) >> 24) == 127 && localaddr.s_addr)
+			address.sin_addr.s_addr = localaddr.s_addr;
 
 		// Call HandleMessage with the remaining content
 		HandleMessage (packet + 4, nb_bytes - 4, &address);
@@ -714,7 +915,4 @@ int main (int argc, char* argv [])
 		// Check if some servers have reached their timeout limit
 		CheckTimeouts ();
 	}
-
-	// We can't be there anyhow
-	return EXIT_SUCCESS;
 }
--- dpmaster.dsp
+++ dpmaster.dsp
@@ -42,8 +42,7 @@ RSC=rc.exe
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /c
-# ADD CPP /nologo /W3 /GX /O2 /D "NDEBUG" /D "WIN32" /D "_CONSOLE" /YX /FD /c
-# SUBTRACT CPP /WX
+# ADD CPP /nologo /W3 /WX /GX /O2 /D "NDEBUG" /D "WIN32" /D "_CONSOLE" /YX /FD /c
 # ADD BASE RSC /l 0x40c /d "NDEBUG"
 # ADD RSC /l 0x40c /d "NDEBUG"
 BSC32=bscmake.exe
@@ -67,8 +66,7 @@ LINK32=link.exe
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /GZ /c
-# ADD CPP /nologo /W3 /Gm /GX /ZI /Od /D "_DEBUG" /D "WIN32" /D "_CONSOLE" /YX /FD /GZ /c
-# SUBTRACT CPP /WX
+# ADD CPP /nologo /W3 /WX /Gm /GX /ZI /Od /D "_DEBUG" /D "WIN32" /D "_CONSOLE" /YX /FD /GZ /c
 # ADD BASE RSC /l 0x40c /d "_DEBUG"
 # ADD RSC /l 0x40c /d "_DEBUG"
 BSC32=bscmake.exe
