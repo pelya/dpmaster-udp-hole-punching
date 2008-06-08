95bead4a02ff6d795edd92efd5266b3a75934bf9
molivier <molivier@d7cf8633-e32d-0410-b094-e92efae38249>
2008-06-08 17:26:26 +0000

    Added a few more servers, with different game names and protocol numbers
    git-svn-id: svn://svn.icculus.org/twilight/trunk/dpmaster@8341 d7cf8633-e32d-0410-b094-e92efae38249

--- testsuite/test-multiple_game_families.pl
+++ testsuite/test-multiple_game_families.pl
@@ -4,16 +4,66 @@ use strict;
 use testlib;
 
 
-# DarkPlaces
-my $dpServerRef = Server_New (GAME_FAMILY_DARKPLACES);
-Server_SetProperty ($dpServerRef, "id", "DPServer");
+my @serverPropertiesList = (
+	# DarkPlaces servers
+	{
+		family => GAME_FAMILY_DARKPLACES,
+		id => "DPServer1",
+		protonum => 1,
+		game => "Game1",
+	},
+	{
+		family => GAME_FAMILY_DARKPLACES,
+		id => "DPServer2",
+		protonum => 2,
+		game => "Game1",
+	},
+	{
+		family => GAME_FAMILY_DARKPLACES,
+		id => "DPServer3",
+		protonum => 1,
+		game => "Game1",
+	},
+	{
+		family => GAME_FAMILY_DARKPLACES,
+		id => "DPServer3",
+		protonum => 1,
+		game => "Game2",
+	},
 
-my $dpClientRef = Client_New (GAME_FAMILY_DARKPLACES);
+	# Q3A servers
+	{
+		family => GAME_FAMILY_QUAKE3ARENA,
+		id => "Q3Server1",
+		protonum => 2,
+	},
+	{
+		family => GAME_FAMILY_QUAKE3ARENA,
+		id => "Q3Server2",
+		protonum => 1,
+	},
+	{
+		family => GAME_FAMILY_QUAKE3ARENA,
+		id => "Q3Server3",
+		protonum => 2,
+	},
+);
 
-# Quake 3 Arena
-my $q3ServerRef = Server_New (GAME_FAMILY_QUAKE3ARENA);
-Server_SetProperty ($q3ServerRef, "id", "Q3Server");
+foreach my $propertiesRef (@serverPropertiesList) {
+	my $serverFamily = $propertiesRef->{family};
+	my $serverProtocol = $propertiesRef->{protonum};
+	my $serverGame = $propertiesRef->{game};
 
-my $q3ClientRef = Client_New (GAME_FAMILY_QUAKE3ARENA);
+	# Create the server
+	my $serverRef = Server_New ($serverFamily);
+	Server_SetProperty ($serverRef, "id", $propertiesRef->{id});
+	Server_SetGameProperty ($serverRef, "protocol", $serverProtocol);
+	Server_SetGameProperty ($serverRef, "gamename", $serverGame);
+
+	# Create the associated client
+	my $clientRef = Client_New ($serverFamily);
+	Client_SetGameProperty ($clientRef, "protocol", $serverProtocol);
+	Client_SetGameProperty ($clientRef, "gamename", $serverGame);
+}
 
 Test_Run ("Servers running games from different game families");
